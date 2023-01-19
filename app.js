ae7be043ad2ab7c29d8d74313a776fcf88ae5b97
Sepulven <sepulven40028922@gmail.com>
2023-01-19 00:23:19 +0000

    Fineshed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const functions = require('daisyui/src/colors/functions.js')
 const express = require('express')
 const path = require('path')
 const app = express()
@@ -10,6 +11,6 @@ app.use(express.static(path.join(__dirname, 'statics')))
 
 app.use(router)
 
-app.use((req, res, next) => { res.sendFile(path.join(__dirname, '/statics/error.html')) });
+app.use((req, res, next) => { res.sendFile(path.join(__dirname, '/statics/404.html')) });
 
 app.listen(PORT, () => { console.log(`Already started at localhost:${PORT}`) })
